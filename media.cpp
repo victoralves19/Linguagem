0bdff9a80a6671a564554d422f21fd84d4b0aecc
victoralves19 <victor.alves.112@ufrn.edu.br>
2026-04-22 13:59:19 +0000

    Atividade 1.1. Manipulando entrada e saída com C++

--- media.cpp
+++ media.cpp
@@ -1,26 +1,25 @@
 #include <iostream>
 #include <iomanip>
 
+float CalcularMedia(float n1, float n2, float n3) {
+    return (n1 + n2 + n3) / 3.0;
+}
+
 int main() {
-    float n = 0;
     float n1, n2, n3;
-    float media;
 
     std::cout << "Entre com a nota da unidade 1: ";
     std::cin >> n1;
-    n++;
 
     std::cout << "Entre com a nota da unidade 2: ";
     std::cin >> n2;
-    n++;
 
     std::cout << "Entre com a nota da unidade 3: ";
     std::cin >> n3;
-    n++;
 
-    media = (n1 + n2 + n3) / n;
-
-    std::cout << "Média: " << std::setprecision(3) << media << std::endl;
+    float media = CalcularMedia(n1, n2, n3);
+    
+    std::cout << "Média: " << std::fixed << std::setprecision(2) << media << std::endl;
 
     return 0;
 }
